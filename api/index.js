a1f13c26324070cd22e78db0c78f95917c8c4758
Riyansh Prakash <riyanshprakash007@gmail.com>
2025-10-01 14:54:45 +0530

    fix: Use CommonJS for Vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,22 +1,8 @@
 // Vercel serverless function that wraps the Express app
 // This file is used by Vercel to deploy the backend as a serverless function
 
-// Import the Express app from server/server.js
-// Note: We need to use dynamic import since server.js uses ES modules
-import('../server/server.js').then(module => {
-  // The Express app is exported as default
-  const app = module.default || module;
+// Import the Express app from server/server.js (CommonJS)
+const app = require('../server/server.js');
 
-  // Export for Vercel
-  if (typeof module.exports !== 'undefined') {
-    module.exports = app;
-  }
-}).catch(err => {
-  console.error('Failed to load server:', err);
-});
-
-// For ES module compatibility
-export default async (req, res) => {
-  const { default: app } = await import('../server/server.js');
-  return app(req, res);
-};
+// Export for Vercel serverless functions
+module.exports = app;
